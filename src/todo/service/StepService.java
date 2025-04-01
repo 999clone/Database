ca529b20a018c5ab2b95e988b2758a526d52969c
shervin <shervinrajabi2016@gmail.com>
2025-04-01 18:38:20 +0330

    updated Errors.

--- src/todo/service/StepService.java
+++ src/todo/service/StepService.java
@@ -1,36 +1,70 @@
 package todo.service;
 
 import db.Database;
+import db.Entity;
 import todo.entity.Step;
 import todo.validator.StepValidator;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class StepService {
 
     public static void addStep(Scanner scanner) {
-        System.out.println("Enter task ID: ");
-        int taskRef = scanner.nextInt();
+        try {
+            System.out.println("Enter task ID: ");
+            int taskRef = scanner.nextInt();
 
-        System.out.println("Enter Title: ");
-        String title = scanner.nextLine();
+            scanner.nextLine();
 
+            System.out.println("Enter Title: ");
+            String title = scanner.nextLine();
+
+            Step step = new Step();
+            step.setTitle(title);
+            step.setTaskRef(taskRef);
+
+            checkStepValidity(step);
+
+            saveStep(step);
+        } catch (InputMismatchException | NumberFormatException e) {
+            System.out.println("Invalid input for task ID. Please enter a valid integer.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Cannot add step.");
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
 
-    public static void checkStepValidity(int taskRef, String title) {
+    public static void checkStepValidity(Step step) {
         StepValidator validator = new StepValidator();
-        Step step = new Step();
-        step.setTitle(title);
-        step.setTaskRef(taskRef);
         validator.validate(step);
+
     }
 
-    public static void saveStep(int taskRef, String title) {
-        Step step = new Step();
-        step.setTaskRef(taskRef);
-        step.setTitle(title);
+    public static void saveStep(Step step) {
         step.setStatus(Step.Status.NOT_STARTED);
         Database.add(step);
+        System.out.println("Step saved successfully.");
+        System.out.println("ID: " + step.getTaskRef());
+        System.out.println("Creation Date: " + step.getCreationDate());
+    }
+
+    public static void deleteStepsOfTask(int id) {
+        try {
+            Step step = new Step();
+            ArrayList<Entity> steps = Database.getAll(step.getEntityCode());
+            for (Entity stepEntity : steps) {
+                step = (Step) stepEntity;
+                if (step.getTaskRef() == id) {
+                    Database.delete(step.getTaskRef());
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error while deleting steps of task with ID=" + id + ": " + e.getMessage());
+
+        }
     }
 }
--- src/todo/validator/StepValidator.java
+++ src/todo/validator/StepValidator.java
@@ -7,15 +7,15 @@ public class StepValidator implements Validator {
     @Override
     public void validate(Entity entity){
         if (! (entity instanceof Step))
-            throw new IllegalArgumentException("entity is not a Step");
+            throw new IllegalArgumentException("ERROR: entity is not a Step");
 
         Step step = (Step) entity;
 
         if (step.getTitle() == null || step.getTitle().isEmpty())
-            throw new IllegalArgumentException("step title is empty");
+            throw new IllegalArgumentException("ERROR: step title is empty");
 
         if (Database.get(step.getTaskRef()) == null) {
-            throw new IllegalArgumentException("task ref is null");
+            throw new IllegalArgumentException("ERROR: Cannot find task with ID: " + step.getTaskRef());
         }
 
     }
